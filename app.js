3c1e429a4c3dff816f2ef2dd53ced0b33ec78e89
Cameron Quicksall <cameron.quicksall70@gmail.com>
2021-12-08 16:53:00 -0600

    generated html

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
 console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('=============');
+const [name, github] = profileDataArgs;
 
-    //Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+console.log(name, github);
 
-printProfileData(profileDataArgs);
+const pageHTML = generatePage(name, github);
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
